386755d955ef7695c0497d0ae4d126ae24c9ae7d
Eugene <eugenevdm@gmail.com>
2021-09-01 09:38:13 +0200

    test jira integration

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -4,6 +4,24 @@ namespace Mpociot\ChuckNorrisJokes;
 
 class JokeFactory
 {
+    protected $jokes = [
+        'haha',
+        'lol',
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) 
+        {
+            $this->jokes = $jokes;
+        }        
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+
     public function hello()
     {
         echo 'chuck norris joke goes here' . PHP_EOL;
--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -2,6 +2,7 @@
 
 namespace FintechSystems\VirtualminApi\Tests;
 
+use FintechSystems\VirtualminApi\Api;
 use PHPUnit\Framework\TestCase;
 
 class ApiTest extends TestCase
@@ -9,6 +10,8 @@ class ApiTest extends TestCase
     /** @test */
     public function it_checks_if_the_debugger_is_working()
     {
+        $api = new Api;
+        $api->debug();
         $this->assertTrue(true);
     }
 }
